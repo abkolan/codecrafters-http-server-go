b09531cc254c139354c9163ee210d94a5dfa156d
ab <ab@abs-MacBook-Air.local>
2023-10-21 12:43:07 +0530

    fixed reading the connection

--- app/server.go
+++ app/server.go
@@ -12,12 +12,6 @@ func closeListener(ln net.Listener) {
 	}
 }
 
-func closeConnection(conn net.Conn) {
-	if err := conn.Close(); err != nil {
-		fmt.Println("Error while closing the connection", conn)
-	}
-}
-
 func main() {
 	fmt.Println("Starting server..")
 	ln, err := net.Listen("tcp", ":4221")
@@ -34,10 +28,27 @@ func main() {
 	if err != nil {
 		fmt.Println("Connection Error", err)
 	}
-	defer closeConnection(conn)
-	_, err = conn.Write([]byte("HTTP/1.1 200 OK\\r\\n\\r\\n"))
-	if err != nil {
-
-	}
+	handleConnection(conn)
 	os.Exit(0)
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	// Create a byte array that would serve as a buffer
+	buf := make([]byte, 512)
+
+	// Read from the connection
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading from the connection:", err)
+	}
+	message := string(buf[:n])
+	fmt.Printf("Read: %v from the server", message)
+
+	// Respond with a 200
+	resp := "HTTP/1.1 200 OK\\r\\n\\r\\n"
+	_, err = conn.Write([]byte(resp))
+	if err != nil {
+		fmt.Println("Error while reading from the connection", err)
+	}
 }
